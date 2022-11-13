e213e9cdd3789c0f552b9f58d3afa7eb34f11f07
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2022-11-13 17:21:40 +0700

    Update mailer.js

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -20,7 +20,7 @@ const sendEmail = async (email, subject, text) => {
 		text: text
 	};
 
-	if (constants.NODE_ENV !== 'DEPLOYMENT') {
+	if (constants.NODE_ENV !== 'PRODUCTION') {
 		await transporter.sendMail(mailOptions);
 	} else {
 		console.log('\x1b[36m%s\x1b[0m', 'Verified Token: ' + verifiedtoken);
