2bf2e76f1cca1be0fba6aee5bbd8470c729a99e7
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2022-11-13 17:41:15 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,7 @@ process.on('uncaughtException', err => {
 dotenv.config();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  origin: 'http://localhost:3000',
+  origins: ['https://chatala.vercel.app','http://localhost:3000'],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
