234719e76dfe586f49860ddde9e9991ebf43ad95
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-11-13 16:05:46 +0700

    hehe

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const authMiddleware = require("../middlewares/auth");
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
 const {
   friendReq,
   friendAccept,
   friendDecline,
   block,
   unblock,
-} = require("../controllers/friendControllers");
+} = require('../controllers/friendControllers');
 
 const router = express.Router();
 
-router.route("/request/:id").post(authMiddleware, friendReq);
-router.route("/accept/:id").post(authMiddleware, friendAccept);
-router.route("/decline/:id").post(authMiddleware, friendDecline);
-router.route("/block/:id").post(authMiddleware, block);
-router.route("/unblock/:id").post(authMiddleware, unblock);
+router.route('/request/:id').post(authMiddleware, friendReq);
+router.route('/accept/:id').post(authMiddleware, friendAccept);
+router.route('/decline/:id').post(authMiddleware, friendDecline);
+router.route('/block/:id').post(authMiddleware, block);
+router.route('/unblock/:id').post(authMiddleware, unblock);
 
 module.exports = router;
