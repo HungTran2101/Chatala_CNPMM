4f38fe5d6ca098c2151a1f1d1d8d053426f9d73f
Ngọc Đức <70329271+ngocducpham@users.noreply.github.com>
2022-11-20 22:52:53 +0700

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -114,7 +114,7 @@ const loginUser = asyncHandler(async (req, res, next) => {
 });
 
 const logout = asyncHandler(async (req, res, next) => {
-  await Users.findOneAndUpdate({_id: req.user._id},{ online: false });
+  await Users.findByIdAndUpdate(req.user._id,{ online: false });
   res.clearCookie('token');
   res.clearCookie('UID');
   res.status(200).json({
