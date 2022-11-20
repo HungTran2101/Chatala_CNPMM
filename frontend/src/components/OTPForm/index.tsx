a88cf0aa342663f19f242e76c7a14bea33fa178f
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-11-20 23:16:55 +0700

    Update index.tsx

--- frontend/src/components/OTPForm/index.tsx
+++ frontend/src/components/OTPForm/index.tsx
@@ -1,44 +1,38 @@
-import FormTemplate from "../Global/FormTemplate";
-import { Formik, ErrorMessage } from "formik";
-import Link from "next/link";
-import * as S from "./OTPForm.styled";
-import { withRouter, useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useGlobalContext } from "../../contexts/globalContext";
-import { UsersApi } from "../../services/api/users";
-import { API_URL } from "../../services/api/urls";
-
-type User = {
-  name: string;
-  phone: string;
-  password: string;
-};
+import FormTemplate from '../Global/FormTemplate';
+import { Formik, ErrorMessage } from 'formik';
+import Link from 'next/link';
+import * as S from './OTPForm.styled';
+import { withRouter, useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useGlobalContext } from '../../contexts/globalContext';
+import { UsersApi } from '../../services/api/users';
+import { API_URL } from '../../services/api/urls';
 
 const OTPCode = (props: any) => {
-  const [checkError, setCheckError] = useState("false");
+  const [checkError, setCheckError] = useState('false');
   const [countdown, setCountdown] = useState(30);
   const [hiddenButton, setHiddenButton] = useState(true);
-  const router = useRouter()
+  const router = useRouter();
   const initialValues = {
-    verifiedtoken: "",
+    verifiedtoken: '',
   };
 
   const context = useGlobalContext();
 
   const handleSubmit = async (values: any, { setSubmitting }: any) => {
     try {
-      const result = await UsersApi.otpRegister(values)
-      if (result ) {
-        router.push('/login')
+      const result = await UsersApi.otpRegister(values);
+      if (result) {
+        router.push('/login');
       }
     } catch (error) {
-      alert('error')
+      alert('error');
     }
   };
 
   useEffect(() => {
     if (!props.router.query.phoneVerify) {
-      router.replace("/register");
+      router.replace('/register');
     }
   }, []);
 
@@ -56,25 +50,25 @@ const OTPCode = (props: any) => {
 
   return (
     <FormTemplate>
-      <Link href="/register">
+      <Link href='/register'>
         <span>
           <S.BackIcon />
         </span>
       </Link>
-      <S.Suggest>Make sure your phone number is real!</S.Suggest>
+      <S.Suggest>Make sure your Email is real!</S.Suggest>
       <S.Notify>
-        Please check verification OTP code sent to your phone and write below
+        Please check verification OTP code sent to your email and write below
       </S.Notify>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {({ isSubmitting, setSubmitting }) => (
           <S.NewForm>
             <S.SetWidth>
               <S.Input
-                placeholder="Verification OTP code"
-                name="verifiedtoken"
+                placeholder='Verification OTP code'
+                name='verifiedtoken'
                 checkerror={checkError}
               />
-              {checkError === "true" && <S.ErrorMsg>Incorrect otp!</S.ErrorMsg>}
+              {checkError === 'true' && <S.ErrorMsg>Incorrect otp!</S.ErrorMsg>}
               <S.CountDown>Time Remaining: {countdown}s</S.CountDown>
               <S.ButtonVerify
                 hidden={hiddenButton}
@@ -82,10 +76,10 @@ const OTPCode = (props: any) => {
               >
                 <S.CheckPhoneNumber>
                   <p>Not receive OTP code?</p>
-                  <p>Please check your phone number again!</p>
+                  <p>Please check your email again!</p>
                 </S.CheckPhoneNumber>
               </S.ButtonVerify>
-              <S.Button type="submit" disabled={isSubmitting ? true : false}>
+              <S.Button type='submit' disabled={isSubmitting ? true : false}>
                 Verify
               </S.Button>
             </S.SetWidth>
